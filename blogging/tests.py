e8f35f4c697d9cce4f12a5f8524238529ba1b054
thelastdB <finley.ryan@gmail.com>
2022-02-04 05:08:23 -0800

    Adding stub blogging view.

---
+++ blogging/tests.py
@@ -0,0 +1,24 @@
+from unittest import expectedFailure
+from django.test import TestCase
+from django.contrib.auth.models import User
+from blogging.models import Post, Category
+
+class PostTestCase(TestCase):
+    fixtures = ['blogging_test_fixture.json', ]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+
+    def test_string_representation(self):
+        expected = "This is a title"
+        p1 = Post(title=expected)
+        actual = str(p1)
+        self.assertEqual(expected, actual)
+
+class CategoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        expected = "A Category"
+        c1 = Category(name=expected)
+        actual = str(c1)
+        self.assertEqual(expected, actual)
