09b93ede9fd18116ae84b9ea2813164b7557c5ab
thelastdB <finley.ryan@gmail.com>
2022-02-08 05:22:35 -0800

    WIP updates to admin.py to manage Categories from Posts.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInline(admin.TabularInline):
+    model = Category
+    extra = 1
+    # fk_name = "posts"
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    fields = ('name', 'text',)
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    inlines = [
+        CategoryInline,
+    ]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
